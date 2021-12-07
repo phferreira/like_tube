81d3c1fa56cd2bab21c54bd377b0823f09b095bb
phferreira <p.h.ferreirah@gmail.com>
2021-12-07 19:46:20 -0300

    Incluido teste para usecase

---
+++ test/app/modules/home/domain/usecases/implementation/get_video_by_description_usecase_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:like_tube/app/core/errors/i_failure.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video_model.dart';
+import 'package:like_tube/app/modules/home/domain/repositories/i_get_video_repository.dart';
+import 'package:like_tube/app/modules/home/external/connections/errors/failure.dart';
+import 'package:mocktail/mocktail.dart';
+
+class GetVideoByDescriptionRepositoryMock extends Mock implements IGetVideoByDescriptionRepository {}
+
+void main() {
+  IGetVideoByDescriptionRepository repositoryMock = GetVideoByDescriptionRepositoryMock();
+
+  test('Deve retornar um Right()', () async {
+    when(() => repositoryMock(any())).thenAnswer((_) async => const Right([]));
+    final result = await repositoryMock('teste');
+
+    expect(result.isRight(), true);
+    verify(() => repositoryMock(any()));
+  });
+
+  test('Deve retornar um Left(IFailure)', () async {
+    when(() => repositoryMock(any())).thenAnswer((_) async => Left(ApiConnectionError()));
+    final result = await repositoryMock('teste');
+
+    expect(result.isLeft(), true);
+    expect(result, isA<Left<IFailure, List<VideoModel>>>());
+    verify(() => repositoryMock(any()));
+  });
+}
