079a2244f8e448fe79daa3f7b28483478f963b70
phferreira <p.h.ferreirah@gmail.com>
2021-12-07 21:04:55 -0300

    Incluido testes para repository

---
+++ test/app/modules/home/infrastructure/repositories/get_video_by_description_repository_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:like_tube/app/core/errors/i_failure.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video_model.dart';
+import 'package:like_tube/app/modules/home/external/connections/errors/failure.dart';
+import 'package:like_tube/app/modules/home/infrastructure/datasources/i_get_video_by_description_datasource.dart';
+import 'package:mocktail/mocktail.dart';
+
+class GetVideoByDescriptionDataSourceMock extends Mock implements IGetVideoByDescriptionDataSource {}
+
+void main() {
+  IGetVideoByDescriptionDataSource datasourceMock = GetVideoByDescriptionDataSourceMock();
+
+  test('Deve retornar um Right(List<VideoModel>)', () async {
+    when(() => datasourceMock(any())).thenAnswer((_) async => const Right([]));
+    final result = await datasourceMock('');
+    expect(result.isRight(), true);
+    expect(result, isA<Right<IFailure, List<VideoModel>>>());
+    verify(() => datasourceMock(any()));
+  });
+
+  test('Deve retornar um Left(ApiConnectionError())', () async {
+    when(() => datasourceMock(any())).thenAnswer((_) async => Left(ApiConnectionError()));
+    final result = await datasourceMock('');
+    expect(result, isA<Left<IFailure, List<VideoModel>>>());
+    verify(() => datasourceMock(any()));
+  });
+
+  test('Deve retornar um Left(TimeOutError())', () async {
+    when(() => datasourceMock(any())).thenAnswer((_) async => Left(TimeOutError()));
+    final result = await datasourceMock('');
+    expect(result, isA<Left<IFailure, List<VideoModel>>>());
+    verify(() => datasourceMock(any()));
+  });
+
+  test('Deve retornar um Left(NotFoundError())', () async {
+    when(() => datasourceMock(any())).thenAnswer((_) async => Left(NotFoundError()));
+    final result = await datasourceMock('');
+    expect(result, isA<Left<IFailure, List<VideoModel>>>());
+    verify(() => datasourceMock(any()));
+  });
+}
