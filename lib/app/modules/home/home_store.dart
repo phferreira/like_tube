3006b51b97ac814ce966782c4958a80a3faff84b
phferreira <p.h.ferreirah@gmail.com>
2021-12-07 23:22:21 -0300

    Incluido controller da pesquisa

--- lib/app/modules/home/home_store.dart
+++ lib/app/modules/home/home_store.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:like_tube/app/modules/home/domain/usecases/i_get_video_by_description_usecase.dart';
@@ -6,8 +7,9 @@ class HomeStore extends NotifierStore<Exception, int> {
   HomeStore() : super(0);
 
   final IGetVideoByDescriptionUsecase usecase = Modular.get();
+  final pesquisarController = TextEditingController();
 
   void getVideoByDescription(String param) {
-    usecase(param);
+    usecase(pesquisarController.text);
   }
 }
