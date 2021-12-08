2e91ba085fc71c20d851c95049e73342b0455b06
phferreira <p.h.ferreirah@gmail.com>
2021-12-07 22:31:19 -0300

    Incluido extensao para retornar um Ifailure a partir do status code

--- lib/app/modules/home/external/connections/errors/failure.dart
+++ lib/app/modules/home/external/connections/errors/failure.dart
@@ -1,5 +1,20 @@
 import 'package:like_tube/app/core/errors/i_failure.dart';
 
+extension ErrorsInt on int {
+  IFailure getFailure([String message = '']) {
+    switch (this) {
+      case 400:
+        return BadRequestError(message);
+      case 404:
+        return NotFoundError(message);
+      case 408:
+        return TimeOutError(message);
+      default:
+        return ApiConnectionError(message);
+    }
+  }
+}
+
 class ApiConnectionError implements IFailure {
   @override
   final String message;
@@ -19,4 +34,11 @@ class NotFoundError implements IFailure {
   final String message;
 
   NotFoundError([this.message = ""]);
+}
+
+class BadRequestError implements IFailure {
+  @override
+  final String message;
+
+  BadRequestError([this.message = ""]);
 }
