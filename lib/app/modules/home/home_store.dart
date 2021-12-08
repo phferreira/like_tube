de7b89465d2ded28911ed7d1e8c7f5aba152a188
phferreira <p.h.ferreirah@gmail.com>
2021-12-08 17:56:11 -0300

    Alimenta a lista de videos do store retornada pela consulta

--- lib/app/modules/home/home_store.dart
+++ lib/app/modules/home/home_store.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video_model.dart';
 import 'package:like_tube/app/modules/home/domain/usecases/i_get_video_by_description_usecase.dart';
 
 class HomeStore extends NotifierStore<Exception, int> {
@@ -8,8 +9,10 @@ class HomeStore extends NotifierStore<Exception, int> {
 
   final IGetVideoByDescriptionUsecase usecase = Modular.get();
   final pesquisarController = TextEditingController();
+  List<VideoModel> listVideoModel = [];
 
-  void getVideoByDescription(String param) {
-    usecase(pesquisarController.text);
+  void getVideoByDescription(String param) async {
+    listVideoModel = (await usecase(pesquisarController.text)).getRight().getOrElse(() => []);
+    update(5);
   }
 }
