0bc64aee0e0ee1ec50f91f67ea17a337b8902379
phferreira <p.h.ferreirah@gmail.com>
2021-12-09 19:36:55 -0300

    Incluido implementaçao de getvideoByDescriptiondatasource

--- lib/app/modules/home/external/datasources/get_video_by_description_datasource.dart
+++ lib/app/modules/home/external/datasources/get_video_by_description_datasource.dart
@@ -1,18 +1,18 @@
 import 'package:fpdart/fpdart.dart';
-import 'package:like_tube/app/core/connections/i_connection_usecase.dart';
+import 'package:like_tube/app/core/connections/i_http.dart';
 import 'package:like_tube/app/core/errors/i_failure.dart';
-import 'package:like_tube/app/modules/home/domain/entities/video_model.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video.dart';
 import 'package:like_tube/app/modules/home/infrastructure/datasources/i_get_video_by_description_datasource.dart';
 
 class GetVideoByDescriptionDataSource extends IGetVideoByDescriptionDataSource {
-  final IConnection connection;
+  final IHttp connection;
 
   GetVideoByDescriptionDataSource({
     required this.connection,
   });
 
   @override
-  Future<Either<IFailure, List<VideoModel>>> call(String param) async {
-    return await connection(param);
+  Future<Either<IFailure, List<Video>>> call(String param) async {
+    return await connection.get(param);
   }
 }
