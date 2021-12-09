7110f877d26f96cb28f197ab2bdf412478d9154f
phferreira <p.h.ferreirah@gmail.com>
2021-12-09 19:47:50 -0300

    Alterado videomodel para video

--- lib/app/modules/home/home_store.dart
+++ lib/app/modules/home/home_store.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
-import 'package:like_tube/app/modules/home/domain/entities/video_model.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video.dart';
 import 'package:like_tube/app/modules/home/domain/usecases/i_get_video_by_description_usecase.dart';
 
-class HomeStore extends NotifierStore<Exception, List<VideoModel>> {
+class HomeStore extends NotifierStore<Exception, List<Video>> {
   HomeStore() : super([]);
   final IGetVideoByDescriptionUsecase _usecase = Modular.get();
   final pesquisarController = TextEditingController();
