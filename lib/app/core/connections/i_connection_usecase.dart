b7eedf8e40e2d9ff4e5aba2a4f5a08dbc8297892
phferreira <p.h.ferreirah@gmail.com>
2021-12-09 19:26:39 -0300

    Fontes renomeados

--- lib/app/core/connections/i_connection_usecase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:like_tube/app/core/errors/i_failure.dart';
-import 'package:like_tube/app/modules/home/domain/entities/video_model.dart';
-
-abstract class IConnection {
-  Future<Either<IFailure, List<VideoModel>>> call(String description);
-}
--- lib/app/modules/home/external/connections/custom_dio.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/native_imp.dart';
-
-class CustomDio extends DioForNative {
-  CustomDio() {
-    options.baseUrl = 'https://www.googleapis.com/youtube/v3';
-    options.connectTimeout = 30000;
-    options.receiveTimeout = 30000;
-  }
-}
