16a6c1c37b3d51c15fe5f20d8c224ea4a4b30a20
phferreira <p.h.ferreirah@gmail.com>
2021-12-09 19:21:47 -0300

    VideoModel passa a herdar da classe abstrata, renomeado arquivo

---
+++ lib/app/modules/home/domain/entities/video.dart
@@ -0,0 +1,22 @@
+import 'i_video.dart';
+
+class Video extends IVideo {
+  Video({
+    required id,
+    required title,
+    required url,
+    favorite,
+  });
+
+  Video.noProperties() {
+    id = '';
+    title = '';
+    url = '';
+  }
+
+  Video.fromJson(Map<String, dynamic> json) {
+    id = json['id']['videoId'];
+    title = json['snippet']['title'];
+    url = json['snippet']['thumbnails']['high']['url'];
+  }
+}
