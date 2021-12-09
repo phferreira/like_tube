f4d3ba94658ae28592652f69bac25b4d663e9751
phferreira <p.h.ferreirah@gmail.com>
2021-12-08 23:44:11 -0300

    Ajustado home_store

--- lib/app/modules/home/home_store.dart
+++ lib/app/modules/home/home_store.dart
@@ -4,9 +4,6 @@ import 'package:flutter_triple/flutter_triple.dart';
 import 'package:like_tube/app/modules/home/domain/entities/video_model.dart';
 import 'package:like_tube/app/modules/home/domain/usecases/i_get_video_by_description_usecase.dart';
 
-class HomeStore extends NotifierStore<Exception, int> {
-  HomeStore() : super(0);
-
 class HomeStore extends NotifierStore<Exception, List<VideoModel>> {
   HomeStore() : super([]);
   final IGetVideoByDescriptionUsecase _usecase = Modular.get();
