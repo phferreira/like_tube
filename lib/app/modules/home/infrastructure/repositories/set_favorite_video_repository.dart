68f1c7edd422b1afc5074f91bb9f314d2fc54649
phferreira <p.h.ferreirah@gmail.com>
2021-12-10 18:57:19 -0300

    Ajustado setfavoritevideo repository e teste para nao atualizar o favorite, datasource passa a fazer este procedimento

--- lib/app/modules/home/infrastructure/repositories/set_favorite_video_repository.dart
+++ lib/app/modules/home/infrastructure/repositories/set_favorite_video_repository.dart
@@ -1,8 +1,8 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:like_tube/app/core/errors/i_failure.dart';
 import 'package:like_tube/app/modules/home/domain/entities/video.dart';
+import 'package:like_tube/app/modules/home/domain/repositories/failures/set_favorite_video_repository_failure.dart';
 import 'package:like_tube/app/modules/home/domain/repositories/i_set_favorite_video_repository.dart';
-import 'package:like_tube/app/modules/home/infrastructure/datasources/failures/set_favorite_video_datasource_failure.dart';
 import 'package:like_tube/app/modules/home/infrastructure/datasources/i_set_favorite_video_datasource.dart';
 
 class SetFavoriteVideoRepository extends ISetFavoriteVideoRepository {
@@ -13,10 +13,12 @@ class SetFavoriteVideoRepository extends ISetFavoriteVideoRepository {
   @override
   Future<Either<IFailure, bool>> call(Video video) async {
     try {
-      video.favorite = await datasource(video);
-      return Right(video.favorite);
+      await datasource(video);
+      return const Right(true);
+    } on IFailure catch (e) {
+      return Left(e);
     } catch (e) {
-      return Left(SetFavoriteVideoDatasourceError());
+      return Left(SetFavoriteVideoRepositoryError());
     }
   }
 }
--- test/app/modules/home/infrastructure/repositories/set_favorite_video_repository_test.dart
+++ test/app/modules/home/infrastructure/repositories/set_favorite_video_repository_test.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
+import 'package:like_tube/app/core/enum/database_result_enum.dart';
 import 'package:like_tube/app/core/errors/i_failure.dart';
 import 'package:like_tube/app/modules/home/domain/entities/video.dart';
-import 'package:like_tube/app/modules/home/infrastructure/datasources/failures/set_favorite_video_datasource_failure.dart';
+import 'package:like_tube/app/modules/home/external/services/errors/database_failure.dart';
 import 'package:like_tube/app/modules/home/infrastructure/datasources/i_set_favorite_video_datasource.dart';
 import 'package:like_tube/app/modules/home/infrastructure/repositories/set_favorite_video_repository.dart';
 import 'package:mocktail/mocktail.dart';
@@ -20,45 +21,19 @@ void main() {
   final repository = SetFavoriteVideoRepository(datasource: datasource);
   final video = Video.noProperties();
 
-  test('Deve retornar um Right(true) e favorite == true quando video.favrite == true', () async {
-    when(() => datasource(any())).thenAnswer((_) async => true);
-    video.favorite = false;
+  test('Deve retornar Right(True) ', () async {
+    when(() => datasource(any())).thenAnswer((_) async => []);
     final result = await repository(video);
     expect(result.isRight(), true);
-    expect(result.getRight().getOrElse(() => false), true);
-    expect(video.favorite, true);
     verify(() => datasource(any()));
   });
 
-  test('Deve retornar um Right(false) e favorite == false quando video.favorite == true', () async {
-    when(() => datasource(any())).thenAnswer((_) async => false);
-    video.favorite = true;
-    final result = await repository(video);
-    expect(result.isRight(), true);
-    expect(result.getRight().getOrElse(() => true), false);
-    expect(video.favorite, false);
-    verify(() => datasource(any()));
-  });
-
-  test('Deve retornar um Left(SetFavoriteVideoDatasourceError) passando com favorite == true e não atualiza o favorite ', () async {
-    when(() => datasource(any())).thenThrow((_) async => SetFavoriteVideoDatasourceError());
-    video.favorite = true;
+  test('Deve retornar um Left(SetFavoriteVideoDatasourceError) a não atualizar favorite ', () async {
+    when(() => datasource(any())).thenThrow(DataBaseError());
     final result = await repository(video);
     expect(result.isLeft(), true);
-    expect(result, isA<Left<IFailure, bool>>());
+    expect(result, isA<Left<IFailure, DatabaseResultEnum>>());
     expect(result.fold(id, id), isA<IFailure>());
-    expect(video.favorite, true);
-    verify(() => datasource(any()));
-  });
-
-  test('Deve retornar um Left(SetFavoriteVideoDatasourceError) passando com favorite == false e não atualiza o favorite ', () async {
-    when(() => datasource(any())).thenThrow((_) async => SetFavoriteVideoDatasourceError());
-    video.favorite = false;
-    final result = await repository(video);
-    expect(result.isLeft(), true);
-    expect(result, isA<Left<IFailure, bool>>());
-    expect(result.fold(id, id), isA<IFailure>());
-    expect(video.favorite, false);
     verify(() => datasource(any()));
   });
 }
