87a797f8ec588ed54f7a22e8261a8158401f24db
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 18:19:39 -0300

    Incluido favoriteVideoStore para controlar a aba favoritos

---
+++ lib/app/modules/home/favorite_video_store.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+import 'package:like_tube/app/core/errors/i_failure.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video.dart';
+import 'package:like_tube/app/modules/home/domain/usecases/i_get_all_favorite_video_usecase.dart';
+import 'package:like_tube/app/modules/home/domain/usecases/implementation/get_all_favorite_video_usecase.dart';
+
+class FavoriteVideoStore extends NotifierStore<IFailure, List<Video>> {
+  FavoriteVideoStore() : super([]);
+
+  IGetAllFavoriteVideoUsecase get _getAllFavoriteUsecase => Modular.get<GetAllFavoriteVideoUsecase>();
+
+  Future getAllFavoriteVideos() async {
+    setLoading(true);
+    update((await _getAllFavoriteUsecase()).fold((l) => [], (r) => r));
+    setLoading(false);
+  }
+}
