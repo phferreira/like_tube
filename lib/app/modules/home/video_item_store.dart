a87603790dd16a9d0bbfb4c1a58b2f30be7f766d
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 18:21:29 -0300

    Ao desfavoritar um video dentro da aba favoritos, passa a atualizar a lista

--- lib/app/modules/home/video_item_store.dart
+++ lib/app/modules/home/video_item_store.dart
@@ -4,16 +4,19 @@ import 'package:like_tube/app/core/errors/i_failure.dart';
 import 'package:like_tube/app/modules/home/domain/entities/video.dart';
 import 'package:like_tube/app/modules/home/domain/usecases/i_set_favorite_video_usecase.dart';
 import 'package:like_tube/app/modules/home/domain/usecases/implementation/set_favorite_video_usecase.dart';
+import 'package:like_tube/app/modules/home/favorite_video_store.dart';
 
 class VideoItemStore extends NotifierStore<IFailure, Video> {
   VideoItemStore() : super(Video.noProperties());
 
   ISetFavoriteVideoUsecase get _usecase => Modular.get<SetFavoriteVideoUsecase>();
+  FavoriteVideoStore get _favoriteVideoStore => Modular.get<FavoriteVideoStore>();
 
   void saveFavorite(Video video) async {
     setLoading(true);
     await _usecase(video);
     update(video);
+    await _favoriteVideoStore.getAllFavoriteVideos();
     setLoading(false);
   }
 }
