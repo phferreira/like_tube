15018bdb348754e35ecea261d92b96b592a98166
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 11:18:09 -0300

    Incluido GetHistoricVideoDatasource implementaçao e teste

---
+++ lib/app/modules/home/external/datasources/get_historic_video_datasource.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:fpdart/fpdart.dart';
+import 'package:like_tube/app/core/connections/i_database.dart';
+import 'package:like_tube/app/core/errors/i_failure.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video.dart';
+import 'package:like_tube/app/modules/home/external/services/failures/database_failure.dart';
+import 'package:like_tube/app/modules/home/infrastructure/datasources/i_get_historic_video_datasource.dart';
+
+class GetHistoricVideoDatasource extends IGetHistoricVideoDatasource {
+  final IDataBase database;
+
+  GetHistoricVideoDatasource({required this.database});
+
+  @override
+  Future<Either<IFailure, List<Video>>> call() async {
+    const String _table = 'tb_historicvideos';
+
+    try {
+      List<Video> _databaseResult = <Video>[];
+
+      (await database.select(_table, [], {})).fold((l) => throw l, (r) {
+        return r.toList().forEach((element) {
+          Video.fromJson(jsonDecode(element.toString()));
+        });
+      });
+      return Right(_databaseResult);
+    } on IFailure catch (e) {
+      return Left(e);
+    } catch (e) {
+      return Left(DataBaseError());
+    }
+  }
+}
---
+++ test/app/modules/home/external/datasources/get_historic_video_datasource_test.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:like_tube/app/core/errors/i_failure.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:like_tube/app/core/connections/i_database.dart';
+import 'package:like_tube/app/modules/home/domain/entities/video.dart';
+import 'package:like_tube/app/modules/home/external/datasources/get_historic_video_datasource.dart';
+import 'package:like_tube/app/modules/home/external/services/failures/database_failure.dart';
+import 'package:like_tube/app/modules/home/infrastructure/datasources/i_get_historic_video_datasource.dart';
+
+class DataBaseMock extends Mock implements IDataBase {}
+
+void main() {
+  final IDataBase database = DataBaseMock();
+  final IGetHistoricVideoDatasource datasource = GetHistoricVideoDatasource(database: database);
+  List<dynamic> _listResult = [];
+
+  setUpAll(() {
+    Video _video = Video(id: '100', title: 'Titulo 01', url: 'http://teste.com');
+    _listResult.add(jsonEncode(_video.toJson()));
+  });
+
+  test('Deve retornar uma List<Video>', () async {
+    when(() => database.select(any(), any(), any())).thenAnswer((_) async => Right(_listResult));
+    final _result = await datasource();
+
+    expect(_result, isA<Right<IFailure, List<Video>>>());
+    verify(() => database.select(any(), any(), any()));
+  });
+
+  test('Deve retornar uma quando database', () async {
+    when(() => database.select(any(), any(), any())).thenThrow(DataBaseError());
+    final _result = await datasource();
+
+    expect(_result, isA<Left<IFailure, List<Video>>>());
+    verify(() => database.select(any(), any(), any()));
+  });
+}
