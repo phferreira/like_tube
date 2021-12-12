0ad39c92b5dd742b091a2ed2c641a0d1cbacd438
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 20:36:03 -0300

    Ajustado VideoPage para exibir o video selecionado

--- lib/app/modules/video/video_page.dart
+++ lib/app/modules/video/video_page.dart
@@ -1,38 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:like_tube/app/modules/home/domain/entities/video.dart';
 
-class VideoPage extends StatelessWidget {
+class VideoPage extends StatefulWidget {
   final Video video;
 
-  const VideoPage({
-    Key? key,
-    required this.video,
-  }) : super(key: key);
+  const VideoPage({Key? key, required this.video}) : super(key: key);
+
+  @override
+  _VideoPageState createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.video.id,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+        controlsVisibleAtStart: true,
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(((MediaQuery.of(context).orientation == Orientation.landscape) ? AppBar() : const Text(''))),
       appBar: AppBar(
-        title: const Text('Visualização'),
+        title: const Text('teste'),
       ),
-      body: Hero(
-        tag: video.id,
-        child: buildImage(),
-      ),
-    );
-  }
-
-  Widget buildImage() {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: Container(
-        width: 200,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(video.url),
-            fit: BoxFit.cover,
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: (MediaQuery.of(context).orientation == Orientation.portrait) ? MediaQuery.of(context).size.height / 1.5 : MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: YoutubePlayer(
+              onEnded: (_) => Modular.to.pop(),
+              controller: _controller,
+              liveUIColor: Colors.amber,
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
