b80a3e78c19901ed583d809f08ef82bcb616f473
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 18:18:40 -0300

    Ao passar para o menu favorito(index == 1) passa a localizar os favoritos

--- lib/app/modules/home/bottom_navigation_store.dart
+++ lib/app/modules/home/bottom_navigation_store.dart
@@ -1,12 +1,19 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:like_tube/app/core/errors/i_failure.dart';
+import 'package:like_tube/app/modules/home/favorite_video_store.dart';
 
 class BottomNavigationStore extends NotifierStore<IFailure, int> {
   BottomNavigationStore() : super(0);
 
+  FavoriteVideoStore get favoriteVideoStore => Modular.get<FavoriteVideoStore>();
+
   void selectedIndex(int index) {
     setLoading(true);
     update(index);
+    if (index == 1) {
+      favoriteVideoStore.getAllFavoriteVideos();
+    }
     setLoading(false);
   }
 
