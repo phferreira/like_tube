687ecb793a8ce8a02c9145a95be4d5e91c32a725
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 10:43:46 -0300

    Ajustado classe Video para implementar IVideo e incluido metodos toJson/fromJson

--- lib/app/modules/home/domain/entities/video.dart
+++ lib/app/modules/home/domain/entities/video.dart
@@ -1,23 +1,45 @@
+import 'dart:convert';
 import 'i_video.dart';
 
-class Video extends IVideo {
+class Video implements IVideo {
+  @override
+  bool favorite = false;
+
+  @override
+  String id = '';
+
+  @override
+  String title = '';
+
+  @override
+  String url = '';
+
   Video({
-    required id,
-    required title,
-    required url,
-    favorite,
+    required this.id,
+    required this.title,
+    required this.url,
+    this.favorite = false,
   });
 
-  Video.noProperties() {
-    id = '';
-    title = '';
-    url = '';
-    favorite = false;
+  Map<String, dynamic> toMap() {
+    return {
+      'favorite': favorite,
+      'id': id,
+      'title': title,
+      'url': url,
+    };
   }
 
-  Video.fromJson(Map<String, dynamic> json) {
-    id = json['id']['videoId'];
-    title = json['snippet']['title'];
-    url = json['snippet']['thumbnails']['high']['url'];
+  factory Video.fromMap(Map<String, dynamic> map) {
+    return Video(
+      favorite: map['favorite'] ?? false,
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      url: map['url'] ?? '',
+    );
   }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Video.fromJson(String source) => Video.fromMap(jsonDecode(source));
 }
