18d4510ee12bca86d50e5ca25ab5cc3d25de79bf
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 17:20:09 -0300

    incluido as dependencias necessarias

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,5 +1,20 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:like_tube/app/modules/home/bottom_navigation_store.dart';
+import 'package:like_tube/app/modules/home/domain/usecases/implementation/get_historic_video_usecase.dart';
+import 'package:like_tube/app/modules/home/domain/usecases/implementation/remove_historic_video_usecase.dart';
+import 'package:like_tube/app/modules/home/domain/usecases/implementation/set_historic_video_usecase.dart';
+import 'package:like_tube/app/modules/home/external/datasources/get_historic_video_datasource.dart';
+import 'package:like_tube/app/modules/home/external/datasources/remove_historic_video_datasource.dart';
+import 'package:like_tube/app/modules/home/external/datasources/set_historic_video_datasource.dart';
 import 'package:like_tube/app/modules/home/external/services/dio_api_connection.dart';
+import 'package:like_tube/app/modules/home/domain/usecases/implementation/set_favorite_video_usecase.dart';
+import 'package:like_tube/app/modules/home/external/datasources/set_favorite_video_datasource.dart';
+import 'package:like_tube/app/modules/home/external/services/hive_database.dart';
+import 'package:like_tube/app/modules/home/infrastructure/repositories/get_historic_video_repository.dart';
+import 'package:like_tube/app/modules/home/infrastructure/repositories/remove_historic_video_repository.dart';
+import 'package:like_tube/app/modules/home/infrastructure/repositories/set_favorite_video_repository.dart';
+import 'package:like_tube/app/modules/home/infrastructure/repositories/set_historic_video_repository.dart';
+import 'package:like_tube/app/modules/home/video_item_store.dart';
 import '../home/home_store.dart';
 import 'domain/usecases/implementation/get_video_by_description_usecase.dart';
 import 'external/datasources/get_video_by_description_datasource.dart';
@@ -10,15 +25,32 @@ import 'home_page.dart';
 class HomeModule extends Module {
   @override
   final List<Bind> binds = [
-    //Store
+    // Store
     Bind.lazySingleton((i) => HomeStore()),
-    //
+    Bind.lazySingleton((i) => VideoItemStore()),
+    Bind.lazySingleton((i) => BottomNavigationStore()),
+    // Services
     Bind.lazySingleton((i) => CustomDio()),
     Bind.lazySingleton((i) => DioApiConnection(dio: i())),
+    Bind.lazySingleton((i) => HiveDatabase()),
+    // DataSources
     Bind.lazySingleton((i) => GetVideoByDescriptionDataSource(connection: i())),
+    Bind.lazySingleton((i) => SetFavoriteVideoDatasource(database: i())),
+    Bind.lazySingleton((i) => GetHistoricVideoDatasource(database: i())),
+    Bind.lazySingleton((i) => SetHistoricVideoDatasource(database: i())),
+    Bind.lazySingleton((i) => RemoveHistoricVideoDatasource(database: i())),
+    // Repositories
     Bind.lazySingleton((i) => GetVideoByDescriptionRepository(datasource: i())),
+    Bind.lazySingleton((i) => SetFavoriteVideoRepository(datasource: i())),
+    Bind.lazySingleton((i) => GetHistoricVideoRepository(datasource: i())),
+    Bind.lazySingleton((i) => SetHistoricVideoRepository(datasource: i())),
+    Bind.lazySingleton((i) => RemoveHistoricVideoRepository(datasource: i())),
+    // Usecases
     Bind.lazySingleton((i) => GetVideoByDescriptionUsecase(repository: i())),
-    //
+    Bind.lazySingleton((i) => SetFavoriteVideoUsecase(repository: i())),
+    Bind.lazySingleton((i) => GetHistoricVideoUsecase(repository: i())),
+    Bind.lazySingleton((i) => SetHistoricVideoUsecase(repository: i())),
+    Bind.lazySingleton((i) => RemoveHistoricVideoUsecase(repository: i())),
   ];
 
   @override
