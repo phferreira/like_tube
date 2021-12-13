3a1b20d0efaa308693e79b6e52150795121ab38a
phferreira <p.h.ferreirah@gmail.com>
2021-12-12 22:01:39 -0300

    Caso selecionado menu historico, busca os videos cadastrados

--- lib/app/modules/home/bottom_navigation_store.dart
+++ lib/app/modules/home/bottom_navigation_store.dart
@@ -2,17 +2,26 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:like_tube/app/core/errors/i_failure.dart';
 import 'package:like_tube/app/modules/home/favorite_video_store.dart';
+import 'package:like_tube/app/modules/home/history_video_store.dart';
 
 class BottomNavigationStore extends NotifierStore<IFailure, int> {
   BottomNavigationStore() : super(0);
 
   FavoriteVideoStore get favoriteVideoStore => Modular.get<FavoriteVideoStore>();
+  HistoryVideoStore get historyVideoStore => Modular.get<HistoryVideoStore>();
 
   void selectedIndex(int index) {
     setLoading(true);
     update(index);
-    if (index == 1) {
-      favoriteVideoStore.getAllFavoriteVideos();
+
+    switch (index) {
+      case 1:
+        favoriteVideoStore.getAllFavoriteVideos();
+        break;
+      case 2:
+        historyVideoStore.getHistoryVideos();
+        break;
+      default:
     }
     setLoading(false);
   }
