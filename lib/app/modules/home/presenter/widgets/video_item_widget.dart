74247939951032e55226dce7df06ba9af55209ed
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-06-19 20:07:28 -0300

    refactor(Change Widget): Chage to use GridTileBar

--- lib/app/modules/home/presenter/widgets/video_item_widget.dart
+++ lib/app/modules/home/presenter/widgets/video_item_widget.dart
@@ -18,81 +18,53 @@ class VideoItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        AspectRatio(
-          aspectRatio: 16 / 9,
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Modular.to.push(
-                    MaterialPageRoute<void>(
-                      builder: (context) => VideoPage(video: video),
-                    ),
-                  );
-                },
-                child: Container(
-                  width: 200,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(video.url),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 10,
-                child: ScopedBuilder<VideoItemStore, IFailure, Video>(
-                  store: store,
-                  onLoading: (_) {
-                    return const SizedBox();
-                  },
-                  onState: (_, video1) {
-                    return IconButton(
-                      iconSize: 40,
-                      color: video.favorite ? Colors.yellow : Colors.yellow.withAlpha(99),
-                      icon: const Icon(Icons.star),
-                      onPressed: () {
-                        store.saveFavorite(video);
-                      },
-                    );
-                  },
-                ),
-              ),
-              Positioned(
-                top: 10,
-                right: 10,
-                child: IconButton(
-                  iconSize: 40,
-                  color: Colors.black,
-                  icon: const Icon(Icons.share_outlined),
-                  onPressed: () {},
-                ),
-              ),
-            ],
-          ),
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: GridTile(
+        header: GridTileBar(
+          backgroundColor: Colors.black45,
+          leading: const Icon(Icons.videocam),
+          title: Text(video.title),
+          trailing: const Icon(Icons.share_outlined),
         ),
-        ListTile(
-          title: Text(
-            video.title,
-            maxLines: 2,
-          ),
-          leading: FutureBuilder<String>(
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return const CircleAvatar(
-                  backgroundColor: Colors.black,
-                );
-              } else {
-                return const SizedBox();
-              }
+        footer: GridTileBar(
+          backgroundColor: Colors.black45,
+          trailing: ScopedBuilder<VideoItemStore, IFailure, Video>(
+            store: store,
+            onLoading: (_) {
+              return const SizedBox();
+            },
+            onState: (_, video1) {
+              return IconButton(
+                color: video.favorite ? Colors.yellow : Colors.yellow.withAlpha(99),
+                icon: const Icon(Icons.star),
+                splashRadius: 16,
+                onPressed: () {
+                  store.saveFavorite(video);
+                },
+              );
             },
           ),
-        )
-      ],
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Modular.to.push(
+              MaterialPageRoute<void>(
+                builder: (context) => VideoPage(video: video),
+              ),
+            );
+          },
+          child: Container(
+            width: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(video.url),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
